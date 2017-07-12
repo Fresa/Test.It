8158fd1360345c765b4c0a323318a34bd8a5e1d6
Fredrik Arvidsson <fresa@fresa.se>
2017-07-12 22:25:32 +0200

    added nuget spec

--- src/Test.It/Properties/AssemblyInfo.cs
+++ src/Test.It/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Test.It")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Provides abstract test functionality in a BDD like style.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Fredrik Arvidsson")]
 [assembly: AssemblyProduct("Test.It")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
