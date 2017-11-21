a74d76efcd05c7fb1c50cf3d59088f3928ae7d1b
Fresa <fresa@fresa.se>
2017-11-21 17:53:19 +0100

    updated the application starter contract to return an hosting environment collection of data

--- src/Test.It/Starters/IApplicationStarter.cs
+++ src/Test.It/Starters/IApplicationStarter.cs
@@ -1,15 +1,16 @@
-﻿using Owin;
+﻿using System.Collections.Generic;
+using Owin;
 using Test.It.AppBuilders;
 
 namespace Test.It.Starters
 {
     public interface IApplicationStarter
     {
-        void Start(IAppBuilder applicationBuilder);
+        IDictionary<string, object> Start(IAppBuilder applicationBuilder);
     }
 
     public interface IApplicationStarter<out TClient>
     {
-        void Start(IAppBuilder<TClient> applicationBuilder);
+        IDictionary<string, object> Start(IAppBuilder<TClient> applicationBuilder);
     }
 }
