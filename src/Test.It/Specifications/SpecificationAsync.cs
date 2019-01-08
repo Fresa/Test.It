ec0b79c292f19e52035edac015d7097baa9358cf
Fredrik Arvidsson <fredrik.arvidsson@betssongroup.com>
2019-01-08 11:26:50 +0100

    add async specification

---
+++ src/Test.It/Specifications/SpecificationAsync.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+
+namespace Test.It.Specifications
+{
+    public abstract class SpecificationAsync
+    {
+        protected async Task SetupAsync()
+        {
+            await GivenAsync();
+            await WhenAsync();
+        }
+
+        protected virtual Task GivenAsync()
+        {
+            return Task.CompletedTask;
+        }
+
+        protected virtual Task WhenAsync()
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
